815d1efd2cd54f527fadd8531be7c9f89ab940f6
Charlotte Leong <charlotte.leong@kcl.ac.uk>
2022-10-25 23:17:22 +0100

    define ThingForm with three fields, extend home view, add tests

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django import forms
+from things.forms import ThingForm
 
-# Create your tests here.
+class TestThingForm(TestCase):
+    def setUp(self):
+        self.form_input = {
+            'name': 'charlie',
+            'description': 'moderately useful',
+            'quantity': 8
+        }
+
+    def test_form_has_required_fields(self):
+        form = ThingForm()
+        self.assertIn('name', form.fields)
+        self.assertIn('description', form.fields)
+        self.assertIn('quantity', form.fields)
+
+    def test_form_accepts_valid_data_entry(self):
+        form = ThingForm(data = self.form_input)
+        self.assertTrue(form.is_valid())
